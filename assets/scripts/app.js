4f9ba66f7c4d1367f88df7c7142ed14eec23e32f
acupoftee <teedia31@gmail.com>
2019-07-23 16:37:48 -0400

    Add Login Event Listener
    Adding modal event listener for login in app.js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,8 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const modalEvents = require('./events/modalEvents')
 $(() => {
   // your JS code goes here
+  $('#logIn').on('click', modalEvents.openLogin)
 })
