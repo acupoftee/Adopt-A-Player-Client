fd6064513794c2579969164bfc2d244b6443c0bb
acupoftee <teedia31@gmail.com>
2019-07-23 18:19:09 -0400

    Refactor modalEvents.js
    Refactored modalEvents.js.

--- assets/scripts/events/modalEvents.js
+++ assets/scripts/events/modalEvents.js
@@ -2,26 +2,11 @@
 const openLogin = () => {
   $('#signInModal').modal('show')
   $('#signUpModal').modal('hide')
-
-  // $('#myModal').is(':visible');
-  // $('#modal').modal('hide');
-
-  // if ($('#signUpModal').is(':visible')) {
-  //   setTimeout(() => {
-  //     $('#signUpModal').modal('hide')
-  //   }, 1000)
-  // }
 }
 
 const openSignUp = () => {
   $('#signUpModal').modal('show')
   $('#signInModal').modal('hide')
-
-  // if ($('#signInModal').is(':visible')) {
-  //   setTimeout(() => {
-  //     $('#signInModal').modal('hide')
-  //   }, 1000)
-  // }
 }
 
 module.exports = {
