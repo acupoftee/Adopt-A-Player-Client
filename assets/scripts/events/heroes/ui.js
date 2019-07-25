0e5fa7b807d385ba4342f17358c3e7f595842494
acupoftee <teedia31@gmail.com>
2019-07-25 09:57:31 -0400

    Remove Console Logs in User/Hero UI Events
    Removed console.log statements.

--- assets/scripts/events/heroes/ui.js
+++ assets/scripts/events/heroes/ui.js
@@ -3,8 +3,6 @@
 const showHeroRosterTemplate = require('../../templates/hero-roster.handlebars')
 
 const getHeroRosterSuccess = data => {
-  console.log(data)
-
   const showHeroRosterHtml = showHeroRosterTemplate({heros: data.heros})
 
   $('.hero-list').html(showHeroRosterHtml)
--- assets/scripts/events/users/ui.js
+++ assets/scripts/events/users/ui.js
@@ -3,8 +3,6 @@
 const showProfileTemplate = require('../../templates/profile-listing.handlebars')
 
 const getProfilesSuccess = data => {
-  console.log(data)
-
   const showProfilesHtml = showProfileTemplate({users: data.users})
 
   $('.hero-list').html(showProfilesHtml)
