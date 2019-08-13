5c69201de97c68c119221edbfed3f27ac083c9ab
Tee <teedia31@gmail.com>
2019-08-13 15:19:55 -0400

    Refactor grabImage

--- assets/scripts/templates/helpers/grabImage.js
+++ assets/scripts/templates/helpers/grabImage.js
@@ -1,141 +1,43 @@
 'use strict'
 
 const baseIconUrl = 'https://d1u1mce87gyfbn.cloudfront.net/hero/name/icon-portrait.png'
-const baseFullUrl = 'https://d1u1mce87gyfbn.cloudfront.net/hero/name/full-portrait.png'
 
 const replaceIconName = newName => baseIconUrl.replace('name', newName)
-const replaceFullName = newName => baseFullUrl.replace('name', newName)
 
 const images = {
-  1: {
-    icon: replaceIconName('ana'),
-    full: replaceFullName('ana')
-  },
-  2: {
-    icon: replaceIconName('ashe'),
-    full: replaceFullName('ashe')
-  },
-  3: {
-    icon: replaceIconName('baptiste'),
-    full: replaceFullName('baptiste')
-  },
-  4: {
-    icon: replaceIconName('bastion'),
-    full: replaceFullName('bastion')
-  },
-  5: {
-    icon: replaceIconName('brigitte'),
-    full: replaceFullName('brigitte')
-  },
-  6: {
-    icon: replaceIconName('dva'),
-    full: replaceFullName('dva')
-  },
-  7: {
-    icon: replaceIconName('doomfist'),
-    full: replaceFullName('doomfist'),
-  },
-  8: {
-    icon: replaceIconName('genji'),
-    full: replaceFullName('genji')
-  },
-  9: {
-    icon: replaceIconName('hanzo'),
-    full: replaceFullName('hanzo')
-  },
-  10: { 
-    icon: replaceIconName('junkrat'),
-    full: replaceFullName('junkrat')
-  },
-  11: { 
-    icon: replaceIconName('lucio'),
-    full: replaceFullName('lucio')
-  },
-  12: { 
-    icon: replaceIconName('mccree'),
-    full: replaceFullName('mccree')
-  },
-  13: { 
-    icon: replaceIconName('mei'),
-    full: replaceFullName('mei')
-  },
-  14: { 
-    icon: replaceIconName('mercy'),
-    full: replaceFullName('mercy')
-  },
-  15: { 
-    icon: replaceIconName('moira'),
-    full: replaceFullName('moira')
-  },
-  16: { 
-    icon: replaceIconName('orisa'),
-    full: replaceFullName('orisa')
-  },
-  17: { 
-    icon: replaceIconName('pharah'),
-    full: replaceFullName('pharah')
-  },
-  18: { 
-    icon: replaceIconName('reaper'),
-    full: replaceFullName('reaper')
-  },
-  19: { 
-    icon: replaceIconName('reinhardt'),
-    full: replaceFullName('reinhardt')
-  },
-  20: { 
-    icon: replaceIconName('roadhog'),
-    full: replaceFullName('roadhog')
-  },
-  21: { 
-    icon: replaceIconName('sigma'),
-    full: replaceFullName('sigma')
-  },
-  22: { 
-    icon: replaceIconName('soldier-76'),
-    full: replaceFullName('soldier-76')
-  },
-  23: { 
-    icon: replaceIconName('sombra'),
-    full: replaceFullName('sombra')
-  },
-  24: { 
-    icon: replaceIconName('symmetra'),
-    full: replaceFullName('symmetra')
-  },
-  25: { 
-    icon: replaceIconName('torbjorn'),
-    full: replaceFullName('torbjorn')
-  },
-  26: { 
-    icon: replaceIconName('tracer'),
-    full: replaceFullName('tracer')
-  },
-  27: { 
-    icon: replaceIconName('widowmaker'),
-    full: replaceFullName('widowmaker')
-  },
-  28: { 
-    icon: replaceIconName('winston'),
-    full: replaceFullName('winston')
-  },
-  29: { 
-    icon: replaceIconName('wrecking-ball'),
-    full: replaceFullName('wrecking-ball')
-  },
-  30: { 
-    icon: replaceIconName('zarya'),
-    full: replaceFullName('zarya')
-  },
-  31: { 
-    icon: replaceIconName('zenyatta'),
-    full: replaceFullName('zenyatta')
-  }
+  1: replaceIconName('ana'),
+  2: replaceIconName('ashe'),
+  3: replaceIconName('baptiste'),
+  4: replaceIconName('bastion'),
+  5: replaceIconName('brigitte'),
+  6: replaceIconName('dva'),
+  7: replaceIconName('doomfist'),
+  8: replaceIconName('genji'),
+  9: replaceIconName('hanzo'),
+  10: replaceIconName('junkrat'),
+  11: replaceIconName('lucio'),
+  12: replaceIconName('mccree'),
+  13: replaceIconName('mei'),
+  14: replaceIconName('mercy'),
+  15: replaceIconName('moira'),
+  16: replaceIconName('orisa'),
+  17: replaceIconName('pharah'),
+  18: replaceIconName('reaper'),
+  19: replaceIconName('reinhardt'),
+  20: replaceIconName('roadhog'),
+  21: replaceIconName('sigma'),
+  22: replaceIconName('soldier-76'),
+  23: replaceIconName('sombra'),
+  24: replaceIconName('symmetra'),
+  25: replaceIconName('torbjorn'),
+  26: replaceIconName('tracer'),
+  27: replaceIconName('widowmaker'),
+  28: replaceIconName('winston'),
+  29: replaceIconName('wrecking-ball'),
+  30: replaceIconName('zarya'),
+  31: replaceIconName('zenyatta')
 }
 
-const grabImage = (id, full=false) => {
-  const image = images[id]
-  return full ? image.icon : image.full
-}
+const grabImage = id => images[id]
 
 module.exports = grabImage
