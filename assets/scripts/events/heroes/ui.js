7757beaf7bdbd28c1f5c9a1b1249c5f15488424b
Tee <teedia31@gmail.com>
2019-08-13 15:19:07 -0400

    Add Hero Description Template in Hero Roster UI

--- assets/scripts/events/heroes/ui.js
+++ assets/scripts/events/heroes/ui.js
@@ -5,6 +5,8 @@ const showHeroRosterTemplate = require('../../templates/hero-roster.handlebars')
 
 const showHeroUserTemplate = require('../../templates/profile-listing.handlebars')
 
+const showHeroDescription = require('../../templates/hero/hero-description.handlebars')
+
 const getHeroRosterSuccess = data => {
   const showHeroRosterHtml = showHeroRosterTemplate({heros: data.heros})
   $('.hero-list').html(showHeroRosterHtml)
@@ -15,12 +17,13 @@ const getHeroRosterFailure = () => {
 }
 
 const getUsersByHeroSuccess = data => {
+  const showHeroDescriptionHtml = showHeroDescription({hero: data.hero})
   if (data.hero.users.length === 0) {
-    $('.hero-list').html('<p class="no-one">No one has played this hero yet!</p>').css('color', 'white')
+    $('.hero-list').html(showHeroDescriptionHtml + '<p class="no-one">No one has played this hero yet!</p>').css('color', 'white')
   } else {
     const showUsersByHeroHtml = showHeroUserTemplate({users: data.hero.users})
     const heroHeading = `<h4 class="hero-heading">Showing Results for <span>${data.hero.name}</span></h4>`
-    $('.hero-list').html(heroHeading + showUsersByHeroHtml)
+    $('.hero-list').html(heroHeading + showHeroDescriptionHtml + showUsersByHeroHtml)
   }
 }
 module.exports = {
