59c6add958fe697d87e7a0aad79f51e52bd113b4
Tee <teedia31@gmail.com>
2019-08-14 22:15:35 -0400

    Change back button message

--- assets/scripts/events/heroes/ui.js
+++ assets/scripts/events/heroes/ui.js
@@ -18,12 +18,15 @@ const getHeroRosterFailure = () => {
 
 const getUsersByHeroSuccess = data => {
   const showHeroDescriptionHtml = showHeroDescription({hero: data.hero})
+  const backButton = '</br><a class="back-hero-listings" href="#">Back to Hero Search</a>'
   if (data.hero.users.length === 0) {
-    $('.hero-list').html(showHeroDescriptionHtml + '<p class="no-one">No one has played this hero yet!</p>').css('color', 'white')
+    $('.hero-list').html(showHeroDescriptionHtml + '<p class="no-one">No one has played this hero yet!</p>' + backButton)
+    $('.no-one').css('color', 'white')
+    // $('.back-hero-listings').css('color', 'white')
   } else {
     const showUsersByHeroHtml = showHeroUserTemplate({users: data.hero.users})
     const heroHeading = `<h4 class="hero-heading">Showing Players who've played the hero <span>${data.hero.name}</span></h4>`
-    $('.hero-list').html(showHeroDescriptionHtml + heroHeading + showUsersByHeroHtml)
+    $('.hero-list').html(showHeroDescriptionHtml + heroHeading + backButton + showUsersByHeroHtml)
   }
 }
 module.exports = {
